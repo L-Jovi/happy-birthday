025744089146bf089dff9d34ef6723674ae0154b
Eternal_Jovi <eternal.l.jovi@gmail.com>
2023-04-16 19:55:05 +0800

    feat: add birthday text

--- script/main.js
+++ script/main.js
@@ -18,8 +18,11 @@ const fetchData = () => {
         // Check if the iteration is over
         // Run amimation if so
         if ( dataArr.length === dataArr.indexOf(customData) + 1 ) {
+          const audio = new Audio('img/MerryChristmasMr.Lawrence.mp3');
+          audio.play();
+
           animationTimeline();
-        } 
+        }
       });
     });
 };
@@ -58,17 +61,17 @@ const animationTimeline = () => {
     .to(".container", 0.1, {
       visibility: "visible"
     })
-    .from(".one", 0.7, {
+    .from(".one", 1.7, {
       opacity: 0,
       y: 10
     })
-    .from(".two", 0.4, {
+    .from(".two", 1.4, {
       opacity: 0,
       y: 10
     })
     .to(
       ".one",
-      0.7,
+      1.7,
       {
         opacity: 0,
         y: 10
@@ -77,32 +80,28 @@ const animationTimeline = () => {
     )
     .to(
       ".two",
-      0.7,
+      1.7,
       {
         opacity: 0,
         y: 10
       },
       "-=1"
     )
-    .from(".three", 0.7, {
+    .from(".three", 1.7, {
       opacity: 0,
       y: 10
       // scale: 0.7
     })
     .to(
       ".three",
-      0.7,
+      1.7,
       {
         opacity: 0,
         y: 10
       },
       "+=2"
     )
-    .from(".four", 0.7, {
-      scale: 0.2,
-      opacity: 0
-    })
-    .from(".fake-btn", 0.3, {
+    .from(".four", 1.7, {
       scale: 0.2,
       opacity: 0
     })
@@ -114,12 +113,16 @@ const animationTimeline = () => {
       },
       0.05
     )
-    .to(".fake-btn", 0.1, {
+    .from(".fake-btn", 1.3, {
+      scale: 0.2,
+      opacity: 0
+    })
+    .to(".fake-btn", 0.5, {
       backgroundColor: "rgb(127, 206, 248)"
     })
     .to(
       ".four",
-      0.5,
+      1.5,
       {
         scale: 0.2,
         opacity: 0,
@@ -127,23 +130,23 @@ const animationTimeline = () => {
       },
       "+=0.7"
     )
-    .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
+    .from(".idea-1", 1.7, ideaTextTrans)
+    .to(".idea-1", 1.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 1.7, ideaTextTrans)
+    .to(".idea-2", 1.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 1.7, ideaTextTrans)
+    .to(".idea-3 strong", 1.5, {
       scale: 1.2,
       x: 10,
-      backgroundColor: "rgb(21, 161, 237)",
-      color: "#fff"
+      // backgroundColor: "rgb(21, 161, 237)",
+      // color: "#fff"
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-3", 2, ideaTextTransLeave, "+=1.5")
+    .from(".idea-4", 2, ideaTextTrans)
+    .to(".idea-4", 2, ideaTextTransLeave, "+=1.5")
     .from(
       ".idea-5",
-      0.7,
+      2,
       {
         rotationX: 15,
         rotationZ: -10,
@@ -156,7 +159,7 @@ const animationTimeline = () => {
     )
     .to(
       ".idea-5 .smiley",
-      0.7,
+      2,
       {
         rotation: 90,
         x: 8
@@ -165,7 +168,7 @@ const animationTimeline = () => {
     )
     .to(
       ".idea-5",
-      0.7,
+      2,
       {
         scale: 0.2,
         opacity: 0
@@ -174,7 +177,7 @@ const animationTimeline = () => {
     )
     .staggerFrom(
       ".idea-6 span",
-      0.8,
+      2,
       {
         scale: 3,
         opacity: 0,
@@ -185,19 +188,19 @@ const animationTimeline = () => {
     )
     .staggerTo(
       ".idea-6 span",
-      0.8,
+      1.8,
       {
         scale: 3,
         opacity: 0,
         rotation: -15,
         ease: Expo.easeOut
       },
-      0.2,
+      0.5,
       "+=1"
     )
     .staggerFromTo(
       ".baloons img",
-      2.5,
+      3.5,
       {
         opacity: 0.9,
         y: 1400
@@ -210,7 +213,7 @@ const animationTimeline = () => {
     )
     .from(
       ".lydia-dp",
-      0.5,
+      1.5,
       {
         scale: 3.5,
         opacity: 0,
@@ -220,7 +223,7 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .from(".hat", 0.5, {
+    .from(".hat", 1, {
       x: -100,
       y: 350,
       rotation: -180,
@@ -228,7 +231,7 @@ const animationTimeline = () => {
     })
     .staggerFrom(
       ".wish-hbd span",
-      0.7,
+      1.7,
       {
         opacity: 0,
         y: -50,
@@ -241,7 +244,7 @@ const animationTimeline = () => {
     )
     .staggerFromTo(
       ".wish-hbd span",
-      0.7,
+      1.7,
       {
         scale: 1.4,
         rotationY: 150
@@ -252,12 +255,12 @@ const animationTimeline = () => {
         color: "#ff69b4",
         ease: Expo.easeOut
       },
-      0.1,
+      0.5,
       "party"
     )
     .from(
       ".wish h5",
-      0.5,
+      1.5,
       {
         opacity: 0,
         y: 10,
@@ -265,41 +268,53 @@ const animationTimeline = () => {
       },
       "party"
     )
-    .staggerTo(
-      ".eight svg",
-      1.5,
-      {
-        visibility: "visible",
-        opacity: 0,
-        scale: 80,
-        repeat: 3,
-        repeatDelay: 1.4
-      },
-      0.3
-    )
-    .to(".six", 0.5, {
+    // .staggerTo(
+    //   ".eight svg",
+    //   1.5,
+    //   {
+    //     visibility: "visible",
+    //     opacity: 0,
+    //     scale: 80,
+    //     repeat: 3,
+    //     repeatDelay: 1.4,
+    //     zIndex: "-1"
+    //   },
+    //   0.3
+    // )
+    .to(".six", 1.5, {
       opacity: 0,
       y: 30,
       zIndex: "-1"
     })
-    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .staggerFrom(".nine p", 1.5, ideaTextTrans, 1.5)
     .to(
       ".last-smile",
-      0.5,
+      1.5,
       {
         rotation: 90
       },
-      "+=1"
+      "+=2"
+    )
+    .from(
+      ".name-seal",
+      3.5,
+      {
+        scale: 1,
+        opacity: 0,
+        x: 25,
+        y: -25,
+      },
+      "+=2"
     );
 
   // tl.seek("currentStep");
   // tl.timeScale(2);
 
   // Restart Animation on click
-  const replyBtn = document.getElementById("replay");
-  replyBtn.addEventListener("click", () => {
-    tl.restart();
-  });
+  // const replyBtn = document.getElementById("replay");
+  // replyBtn.addEventListener("click", () => {
+  //   tl.restart();
+  // });
 };
 
 // Run fetch and animation in sequence
